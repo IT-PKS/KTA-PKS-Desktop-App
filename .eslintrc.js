ad3d7122fafb8d919b8bfb0b554dac62c806cd12
Bakti Aditya <bakti@bvap.me>
2020-08-19 17:07:32 +0700

    Updating eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -86,6 +86,7 @@ module.exports = {
         '@typescript-eslint/no-use-before-define': 'off',
         '@typescript-eslint/no-unused-vars': 'off',
         '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
         'react/prop-types': 'off',
         'react/no-unused-prop-types': 'off',
       },
