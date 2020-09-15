51bca197539d4b586f71dd7d06c627a4d8a41b69
Dodi <dodipras27@gmail.com>
2020-09-15 20:21:46 +0700

    refactor second

--- app/components/contextual/App.tsx
+++ app/components/contextual/App.tsx
@@ -9,27 +9,31 @@ import initializeSQLite from '../../services/sqlite/initializeSQLite'
 import "reflect-metadata";
 import { User } from "../../entity/User";
 
+const clientTest = async () => {
+  const connection = await initializeSQLite()
+  console.log("Inserting a new user into the database...");
+  const user = new User();
+  user.firstName = "Timber";
+  user.lastName = "Saw";
+  user.age = 25;
+
+
+  await connection.manager.save(user);
+  console.log("Saved a new user with id: " + user.id);
+
+  console.log("Loading users from the database...");
+  const users = await connection.manager.find(User);
+  console.log("Loaded users: ", users);
+
+  console.log("Here you can setup and run express/koa/any other framework.");
+
+}
+
 
 const App: React.FC = () => {
 
   const _getUser = async () => {
-    const connection = await initializeSQLite()
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-
-
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
-
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
-
-    console.log("Here you can setup and run express/koa/any other framework.");
-
+    await clientTest()
   }
 
   useEffect(() => {
