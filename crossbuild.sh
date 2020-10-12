18cc64f46cb115a4b71c5e907fe042c8767cb35f
Dodi <dodipras27@gmail.com>
2020-10-12 14:40:20 +0700

    update release script

--- crossbuild.sh
+++ crossbuild.sh
@@ -6,4 +6,4 @@
  -v ~/.cache/electron:/root/.cache/electron \
  -v ~/.cache/electron-builder:/root/.cache/electron-builder \
  electronuserland/builder:wine \
- /bin/bash -c "yarn cross-env NODE_ENV=production node task-runner.js cp-html-to-dist && yarn cross-env NODE_ENV=production node task-runner.js build-main && yarn cross-env NODE_ENV=production node task-runner.js build-renderer  && yarn cross-env NODE_ENV=production electron-builder $1 $2"
+ /bin/bash -c "yarn cross-env NODE_ENV=production node task-runner.js cp-html-to-dist && yarn cross-env NODE_ENV=production node task-runner.js build-main && yarn cross-env NODE_ENV=production node task-runner.js build-renderer  && yarn cross-env NODE_ENV=production electron-builder $1 $2 $3 $4"
