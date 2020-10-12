b44cd94c83e4c592f4a2d115dbacc950bc3580fd
Dodi <dodipras27@gmail.com>
2020-10-12 21:25:57 +0700

    back to private root

--- app/components/contextual/PrivateRoute.tsx
+++ app/components/contextual/PrivateRoute.tsx
@@ -11,10 +11,8 @@ import TemplateDataProvider from './TemplateDataProvider';
 const PrivateRoute: React.FC<RouteProps> = ({ component, ...rest }) => {
   const { user, serialKey, finishChecking, onLogout } = useAuthDataContext();
 
-  // const alReadyHaveSerialKey = user ? component : SignInPage;
-  // const finalComponent = serialKey ? LicenseKey : alReadyHaveSerialKey;
-  const finalComponent = component;
-
+  const alReadyHaveSerialKey = user ? component : SignInPage;
+  const finalComponent = serialKey ? LicenseKey : alReadyHaveSerialKey;
 
   if (finishChecking === false) {
     return <div>Authenticating..</div>;
