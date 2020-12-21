7dc59a387996338a50e9057e42e10b655955e4c9
Dodi <dodipras27@gmail.com>
2020-12-21 21:18:05 +0700

    save to local upload image

--- app/client/RegisterClient.ts
+++ app/client/RegisterClient.ts
@@ -1,6 +1,9 @@
 import initSQLite from '../services/sqlite/initSQLite';
 import { Member } from "../entity/Member";
 import { clientPost } from '../services/URLApi/URLApi';
+import { ipcRenderer, remote } from 'electron';
+const { app } = remote
+import fs from 'fs'
 
 export const saveToLocal = async (payload: any, data: any) => {
     const sqlite: any = await initSQLite()
@@ -10,9 +13,23 @@ export const saveToLocal = async (payload: any, data: any) => {
         payload['isSentToBackend'] = false
     }
     try {
+        // copy file (offline upload)
+        const sourceKTP = payload.ktp_file[0].path
+        const sourceProfilePicture = payload.profile_file[0].path
+        const destination = app.getPath('userData') + '/'
+        fs.copyFile(sourceKTP, destination + payload.ktp_file[0].name, (err) => {
+            if (err) throw err;
+            console.log('file ktp uploaded locally');
+        });
+        fs.copyFile(sourceProfilePicture, destination + payload.profile_file[0].name, (err) => {
+            if (err) throw err;
+            console.log('file profile uploaded locally');
+        });
+
         const member = new Member(payload)
         const res = await sqlite.manager.save(member)
         sqlite.close()
+
         return res
     } catch (error) {
         sqlite.close()
